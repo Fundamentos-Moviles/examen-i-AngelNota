3d7d3b278eeb940a85cfadcf95fcac24640568b7
angelnota <getincine@gmail.com>
2024-09-18 13:39:17 -0600

    more_info

--- lib/more_info.dart
+++ lib/more_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:examen_1_lacm/utils/constants.dart' as con;
 
 class MoreInfo extends StatefulWidget {
   const MoreInfo({super.key});
@@ -8,8 +9,61 @@ class MoreInfo extends StatefulWidget {
 }
 
 class _MoreInfoState extends State<MoreInfo> {
+  List<Widget> boxes = [];
+
+  void _addBox() {
+    setState(() {
+      Color boxColor = boxes.length.isEven ? con.fondo4 : con.fondo2;
+      boxes.add(
+        Container(
+          decoration: BoxDecoration(
+            color: boxColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: con.fondo,
+      appBar: AppBar(
+        backgroundColor: con.fondo2,
+        title: const Text('More Info', style: TextStyle(color: con.blanco)),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 12,
+              padding: const EdgeInsets.all(10),
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              children: boxes,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: _addBox,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: con.botones,
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text(
+                'Agregar recuadro',
+                style: TextStyle(color: con.blanco),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
